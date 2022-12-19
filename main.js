d19e6e019aee25ef5d64f69b56d5347221d57765
İHLASMERT <ihlas1mert@gmail.com>
2022-12-19 22:39:23 +0300

    added resposibility and ended project

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+
+
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+
+
+const getJoke =()=>{
+    fetch(url)
+    .then(data =>data.json())
+    .then(item =>{
+        jokeContainer.textContent = `${item.joke}`
+    })
+}
+btn.addEventListener("click", getJoke)
+getJoke()
